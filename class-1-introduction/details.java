e86a34d44678d0f1ddd0401bda5f3c2094828117
Samarth H Chinivar <samarthharish321@gmail.com>
2023-07-14 22:55:03 +0530

    class-1 commit

---
+++ class-1-introduction/details.java
@@ -0,0 +1,9 @@
+class details {
+	public static void main(String [] args) {
+		System.out.println("Samarth H Chinivar");
+		System.out.println("Sai Vidya Institute of Technology");
+		System.out.println("8971201756");
+		System.out.println("2023 Passout");
+		System.out.println("http://samarth-portfolio-website.000webhostapp.com");	
+	}
+}
