2750fe1c017c5c05275035caa555ff991f1cc874
Samarth H Chinivar <samarthharish321@gmail.com>
2023-07-20 22:01:23 +0530

    variables added

---
+++ class-4-variables/EmpDetails.java
@@ -0,0 +1,12 @@
+class EmpDetails {
+	public static void main(String [] args) {
+		
+		String name="Sharukh";
+		String job="Actor";
+		int empNo = 1;
+
+		System.out.println("Employee's Name :  " + name);
+		System.out.println("Employee's Job : " + job);
+		System.out.println("Employee Number : " + empNo);
+	}
+}
